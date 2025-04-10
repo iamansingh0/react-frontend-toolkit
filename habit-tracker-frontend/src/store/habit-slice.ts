85630c8a1cfc83447a26d5a49e727090dacb61d2
iamansingh0 <amankumar7355197337@gmail.com>
2025-04-10 10:51:53 +0530

    added backend API

--- habit-tracker-frontend/src/store/habit-slice.ts
+++ habit-tracker-frontend/src/store/habit-slice.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://habit-tracker-api-fpas.onrender.com/api";
 
 export interface Habit {
     id: string;
