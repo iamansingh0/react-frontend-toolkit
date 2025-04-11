ce61842177e82536003a3ee0be49b41890aa9477
iamansingh0 <amankumar7355197337@gmail.com>
2025-04-11 14:28:15 +0530

    cors policy

--- habit-tracker-backend/server.js
+++ habit-tracker-backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://habit-tracker-rho-opal.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
